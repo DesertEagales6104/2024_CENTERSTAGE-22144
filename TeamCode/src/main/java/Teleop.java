37fef2c78bdbdd6a45be417da43923b27baaf2e7
Au78zZz <pazyerushalmi@gmail.com>
2024-01-24 15:54:16 +0200

    U1

--- TeamCode/src/main/java/Teleop.java
+++ TeamCode/src/main/java/Teleop.java
@@ -1,10 +1,26 @@
 
 
+import androidx.annotation.NonNull;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.MagneticFlux;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
+import org.firstinspires.ftc.robotcore.external.navigation.Temperature;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 @TeleOp
 public class Teleop extends LinearOpMode {
@@ -19,6 +35,159 @@ public class Teleop extends LinearOpMode {
         DcMotor rightElevator = hardwareMap.dcMotor.get("rightElevator");
         DcMotor leftElevator = hardwareMap.dcMotor.get("leftElevator");
         DcMotor intake = hardwareMap.dcMotor.get("intake");
+        BNO055IMU imu = new BNO055IMU() {
+            @Override
+            public boolean initialize(@NonNull Parameters parameters) {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public Parameters getParameters() {
+                return null;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public Orientation getAngularOrientation() {
+                return null;
+            }
+
+            @Override
+            public Orientation getAngularOrientation(AxesReference reference, AxesOrder order, org.firstinspires.ftc.robotcore.external.navigation.AngleUnit angleUnit) {
+                return null;
+            }
+
+            @Override
+            public Acceleration getOverallAcceleration() {
+                return null;
+            }
+
+            @Override
+            public AngularVelocity getAngularVelocity() {
+                return null;
+            }
+
+            @Override
+            public Acceleration getLinearAcceleration() {
+                return null;
+            }
+
+            @Override
+            public Acceleration getGravity() {
+                return null;
+            }
+
+            @Override
+            public Temperature getTemperature() {
+                return null;
+            }
+
+            @Override
+            public MagneticFlux getMagneticFieldStrength() {
+                return null;
+            }
+
+            @Override
+            public Quaternion getQuaternionOrientation() {
+                return null;
+            }
+
+            @Override
+            public Position getPosition() {
+                return null;
+            }
+
+            @Override
+            public Velocity getVelocity() {
+                return null;
+            }
+
+            @Override
+            public Acceleration getAcceleration() {
+                return null;
+            }
+
+            @Override
+            public void startAccelerationIntegration(Position initialPosition, Velocity initialVelocity, int msPollInterval) {
+
+            }
+
+            @Override
+            public void stopAccelerationIntegration() {
+
+            }
+
+            @Override
+            public SystemStatus getSystemStatus() {
+                return null;
+            }
+
+            @Override
+            public SystemError getSystemError() {
+                return null;
+            }
+
+            @Override
+            public CalibrationStatus getCalibrationStatus() {
+                return null;
+            }
+
+            @Override
+            public boolean isSystemCalibrated() {
+                return false;
+            }
+
+            @Override
+            public boolean isGyroCalibrated() {
+                return false;
+            }
+
+            @Override
+            public boolean isAccelerometerCalibrated() {
+                return false;
+            }
+
+            @Override
+            public boolean isMagnetometerCalibrated() {
+                return false;
+            }
+
+            @Override
+            public CalibrationData readCalibrationData() {
+                return null;
+            }
+
+            @Override
+            public void writeCalibrationData(CalibrationData data) {
+
+            }
+
+            @Override
+            public byte read8(Register register) {
+                return 0;
+            }
+
+            @Override
+            public byte[] read(Register register, int cb) {
+                return new byte[0];
+            }
+
+            @Override
+            public void write8(Register register, int bVal) {
+
+            }
+
+            @Override
+            public void write(Register register, byte[] data) {
+
+            }
+        };
+
         // Servo intakeAngle = hardwareMap.Servo.get("intakeAngle");
 
         // Reverse the right side motors. This may be wrong for your setup.
@@ -45,6 +214,11 @@ public class Teleop extends LinearOpMode {
             //double ia gamepad1.right_bumper;
 
 
+            double botHeading = -imu.getAngularOrientation().firstAngle /*- Math.PI*/;
+
+            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
@@ -56,6 +230,13 @@ public class Teleop extends LinearOpMode {
             double rightElevatorPower =ry;
             double leftElevatorPower=ry;
             double intakePower=i;
+
+            if (gamepad1.circle){
+                BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+                parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+                imu.initialize(parameters);
+
+            }
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
