276258c86e79daabb1b42e7de1bda2634cd82221
Deagles22144 <ftc.deagles22144@gmail.com>
2024-02-14 17:32:18 +0200

    fixed IMU + some servo logic

--- TeamCode/src/main/java/Teleop.java
+++ TeamCode/src/main/java/Teleop.java
@@ -2,7 +2,9 @@
 
 import androidx.annotation.NonNull;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -21,6 +23,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
 import org.firstinspires.ftc.robotcore.external.navigation.Temperature;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp
 public class Teleop extends LinearOpMode {
@@ -28,6 +31,8 @@ public class Teleop extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
@@ -38,161 +43,9 @@ public class Teleop extends LinearOpMode {
         Servo intakeServo = hardwareMap.servo.get("intakeServo");
         Servo rightElevatorServo = hardwareMap.servo.get("rightElevatorServo");
         Servo leftElevatorServo = hardwareMap.servo.get("leftElevatorServo");
+        YawPitchRollAngles robotOrientation;
 
-        // Servo akdadad;
 
-        BNO055IMU imu = new BNO055IMU() {
-            @Override
-            public boolean initialize(@NonNull Parameters parameters) {
-                return false;
-            }
-
-            @NonNull
-            @Override
-            public Parameters getParameters() {
-                return null;
-            }
-
-            @Override
-            public void close() {
-
-            }
-
-            @Override
-            public Orientation getAngularOrientation() {
-                return null;
-            }
-
-            @Override
-            public Orientation getAngularOrientation(AxesReference reference, AxesOrder order, org.firstinspires.ftc.robotcore.external.navigation.AngleUnit angleUnit) {
-                return null;
-            }
-
-            @Override
-            public Acceleration getOverallAcceleration() {
-                return null;
-            }
-
-            @Override
-            public AngularVelocity getAngularVelocity() {
-                return null;
-            }
-
-            @Override
-            public Acceleration getLinearAcceleration() {
-                return null;
-            }
-
-            @Override
-            public Acceleration getGravity() {
-                return null;
-            }
-
-            @Override
-            public Temperature getTemperature() {
-                return null;
-            }
-
-            @Override
-            public MagneticFlux getMagneticFieldStrength() {
-                return null;
-            }
-
-            @Override
-            public Quaternion getQuaternionOrientation() {
-                return null;
-            }
-
-            @Override
-            public Position getPosition() {
-                return null;
-            }
-
-            @Override
-            public Velocity getVelocity() {
-                return null;
-            }
-
-            @Override
-            public Acceleration getAcceleration() {
-                return null;
-            }
-
-            @Override
-            public void startAccelerationIntegration(Position initialPosition, Velocity initialVelocity, int msPollInterval) {
-
-            }
-
-            @Override
-            public void stopAccelerationIntegration() {
-
-            }
-
-            @Override
-            public SystemStatus getSystemStatus() {
-                return null;
-            }
-
-            @Override
-            public SystemError getSystemError() {
-                return null;
-            }
-
-            @Override
-            public CalibrationStatus getCalibrationStatus() {
-                return null;
-            }
-
-            @Override
-            public boolean isSystemCalibrated() {
-                return false;
-            }
-
-            @Override
-            public boolean isGyroCalibrated() {
-                return false;
-            }
-
-            @Override
-            public boolean isAccelerometerCalibrated() {
-                return false;
-            }
-
-            @Override
-            public boolean isMagnetometerCalibrated() {
-                return false;
-            }
-
-            @Override
-            public CalibrationData readCalibrationData() {
-                return null;
-            }
-
-            @Override
-            public void writeCalibrationData(CalibrationData data) {
-
-            }
-
-            @Override
-            public byte read8(Register register) {
-                return 0;
-            }
-
-            @Override
-            public byte[] read(Register register, int cb) {
-                return new byte[0];
-            }
-
-            @Override
-            public void write8(Register register, int bVal) {
-
-            }
-
-            @Override
-            public void write(Register register, byte[] data) {
-
-            }
-        };
 
         // Servo intakeAngle = hardwareMap.Servo.get("intakeAngle");
 
@@ -204,10 +57,18 @@ public class Teleop extends LinearOpMode {
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rightElevator.setDirection(DcMotorSimple.Direction.REVERSE);
         intakeServo.setDirection(Servo.Direction.REVERSE);
-        leftElevatorServo.setPosition(0);
-        rightElevatorServo.setPosition(0);
-        intakeServo.setPosition(1);
-        rightElevatorServo.setDirection(Servo.Direction.REVERSE);
+        leftElevatorServo.setDirection(Servo.Direction.REVERSE);
+
+        IMU.Parameters imuPar = new IMU.Parameters(
+            new RevHubOrientationOnRobot(
+                    RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                    RevHubOrientationOnRobot.UsbFacingDirection.UP
+            )
+        );
+        imu.initialize(imuPar);
+        imu.resetYaw();
+
+
         waitForStart();
 
         if (isStopRequested()) return;
@@ -218,77 +79,69 @@ public class Teleop extends LinearOpMode {
 
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
+            double rx = - gamepad1.right_trigger + gamepad1.left_trigger;
             double ry = gamepad1.right_stick_y;
             double i = gamepad1.right_trigger;
-//            double si = gamepad2.right_stick_y;
-
-            boolean ia = gamepad1.a;
+            robotOrientation = imu.getRobotYawPitchRollAngles();
 
 
-//            double botHeading = -imu.getAngularOrientation().firstAngle /*- Math.PI*/;
-//
-//            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
-//            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+            double botHeading = -robotOrientation.getYaw(AngleUnit.RADIANS);
+            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
-            double rightElevatorPower =ry;
-            double leftElevatorPower=ry;
-            double intakePower=i;
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
 //            intakeServo.scaleRange(0.6,0.8);
-
-            if(gamepad1.a && intakeServo.getPosition()==1)  intakeServo.setPosition(0.645);
-            else if(gamepad1.x&&intakeServo.getPosition()!= 1) intakeServo.setPosition(1);
-
-
-            if(gamepad1.dpad_up && leftElevatorServo.getPosition()==0 && rightElevatorServo.getPosition()==0 ) {leftElevatorServo.setPosition(1); rightElevatorServo.setPosition(1);}
-
-            if(gamepad1.dpad_down && leftElevatorServo.getPosition()!=0 && rightElevatorServo.getPosition()!=0) {leftElevatorServo.setPosition(0); rightElevatorServo.setPosition(0);}
-
-
-//            if(gamepad1.a && intakeServo.getPosition()==0.25)
-//            {
-//                intakeServo.setPosition(0);
-//            }
-//            else if(gamepad1.a&&intakeServo.getPosition()!=0.25)
-//            {
-//             intakeServo.setPosition(0.25);
-//            }
-
-
-            if (gamepad1.circle){
-                BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-                parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
-                imu.initialize(parameters);
-
-            }
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
-            intake.setPower(intakePower);
-            if(ry>0.5){
-                rightElevator.setPower(0.5);
-                leftElevator.setPower(0.5);
 
+            if(gamepad1.a && intakeServo.getPosition()==1) {
+                intakeServo.setPosition(0.645);
+                intake.setPower(1);
             }
-            else{
-                rightElevator.setPower(rightElevatorPower);
-                leftElevator.setPower(leftElevatorPower);
+            else if(gamepad1.x&&intakeServo.getPosition()!= 1) {
+                intakeServo.setPosition(1);
+                intake.setPower(0);
+            }
+
+
+            if(gamepad1.dpad_up) {leftElevatorServo.setPosition(1); rightElevatorServo.setPosition(1);}
+
+            if(gamepad1.dpad_down) {leftElevatorServo.setPosition(0); rightElevatorServo.setPosition(0);}
+
+            if(Math.abs(ry) < 0.05) {
+                rightElevator.setPower(0);
+                leftElevator.setPower(0);
+            } else if(Math.abs(ry) < 0.5) {
+                if(!gamepad1.dpad_up && !gamepad1.dpad_down) {
+                    rightElevatorServo.setPosition(0.3);
+                    leftElevatorServo.setPosition(0.3);
+                }
+                rightElevator.setPower(ry);
+                leftElevator.setPower(ry);
+            } else {
+                if(!gamepad1.dpad_up && !gamepad1.dpad_down) {
+                    rightElevatorServo.setPosition(0.3);
+                    leftElevatorServo.setPosition(0.3);
+                }
+                rightElevator.setPower(Math.copySign(0.5,ry));
+                leftElevator.setPower(Math.copySign(0.5,ry));
             }
 
 
             telemetry.addLine("Servo Angle is: " + intakeServo.getPosition());
             telemetry.addLine("Right Elevator Angle is:" + rightElevatorServo.getPosition());
             telemetry.addLine("Left Elevator Angle is:" + leftElevatorServo.getPosition());
+            telemetry.addLine("IMU is: " + robotOrientation.getYaw(AngleUnit.RADIANS));
             telemetry.update();
         }
     }
